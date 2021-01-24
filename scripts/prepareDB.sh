c9600997efa10d4a319786ca28f3ab4f45ab626f
Toni Hermoso Pulido <toniher@cau.cat>
2021-01-24 20:00:46 +0100

    update files

--- scripts/prepareDB.sh
+++ scripts/prepareDB.sh
@@ -13,5 +13,3 @@ curl -X PUT -d @auth.json http://$USER:$PASSWD@$HOST:$PORT/$DB/_design/_auth
 curl -X PUT -d @list.json http://$USER:$PASSWD@$HOST:$PORT/$DB/_design/list
 
 curl -X PUT -d @search.json http://$USER:$PASSWD@$HOST:$PORT/$DB/_design/search
-
-curl -X PUT -d @coords.json http://$USER:$PASSWD@$HOST:$PORT/$DB/_design/coords
